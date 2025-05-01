e6448289707dee858d57164f50fc08af29cefdaf
Alex Howard <thezanke@gmail.com>
2025-05-01 15:38:53 -0400

    style

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -10,7 +10,7 @@ function LinkButton({
   target?: HTMLAttributeAnchorTarget
 }>) {
   return (
-    <a href={to} target={target} className="p-4 inline-block rounded-4xl text-xl border-2 border-cyan-600 text-purple-600 hover:border-transparent hover:bg-purple-600 hover:text-cyan-400 active:bg-purple-700 text-center cursor-pointer">
+    <a href={to} target={target} className="p-4 inline-block rounded-4xl text-xl border-2 border-cyan-600 text-purple-600 hover:border-transparent hover:bg-purple-600 hover:text-cyan-400 active:bg-purple-700 text-center cursor-pointer hover:italic">
       {children}
     </a>
   )
