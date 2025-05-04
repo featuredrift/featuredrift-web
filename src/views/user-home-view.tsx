9e46afd50572766d12746e888775745c7c175bbc
Alex Howard <thezanke@gmail.com>
2025-05-04 15:56:19 -0400

    refactor: improve typography and spacing in HomeUserView for enhanced readability

--- src/views/user-home-view.tsx
+++ src/views/user-home-view.tsx
@@ -5,19 +5,18 @@ export function HomeUserView({ session }: { session: SessionResponse }) {
   return (
     <>
       <div className="h-full flex items-center justify-center">
-        <div className="inline-flex flex-col text-left text-2xl gap-2 text-purple-600 animate-pulse">
-          <div className="font-bold">
+        <div className="inline-flex flex-col text-left text-3xl gap-1 text-purple-600 animate-pulse">
+          <div className="font-bold text-2xl">
             <span className="text-cyan-500">{session.user.displayName}</span>,
           </div>
           <div className="italic">
-            You have been activated<span className="text-cyan-500">;</span>
-          </div>
-          <div className="italic">
-            <>expect to be </>
+            <>You have been activated</>
+            <span className="text-cyan-500">;</span>
+            <> expect to be </>
             <span className="text-cyan-500">contacted</span>
             <> soon...</>
           </div>
-          <div className="text-right text-lg italic">
+          <div className="text-right text-xl italic">
             — F<span className="text-cyan-500">3</span>tch
           </div>
         </div>
