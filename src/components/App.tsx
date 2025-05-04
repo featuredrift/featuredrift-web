8c5487b28f8cd35cf4b2f3f5fd5f84bb0173c6e8
Alex Howard <thezanke@gmail.com>
2025-05-04 02:33:15 -0400

    refactor: improve layout of UserLoginView and HomeUserView components

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -16,7 +16,7 @@ function LinkButton({
     <a
       href={to}
       target={target}
-      className="p-4 inline-block rounded-4xl text-xl border-2 border-cyan-600 text-purple-600 hover:border-transparent hover:bg-purple-600 hover:text-cyan-400 active:bg-purple-700 text-center cursor-pointer hover:italic"
+      className="p-4 inline-block rounded-4xl text-xl border-2 border-cyan-600 text-purple-600 hover:border-transparent hover:bg-purple-600 hover:text-cyan-400 active:text-cyan-400 active:bg-purple-700 text-center cursor-pointer hover:italic"
     >
       {children}
     </a>
@@ -48,23 +48,46 @@ function SessionLoading() {
 
 function UserLoginView() {
   return (
-    <>
+    <div className="flex flex-col gap-8 justify-center items-center min-h-full">
+      <div className="flex flex-row justify-center align-middle">
+        <img
+          src={art}
+          alt="FeatureDrift AI Art"
+          className="max-h-[50vh] w-auto motion-safe:animate-pulse"
+        />
+      </div>
       <LinkButton to="/auth/login" target="_self">
         Login
       </LinkButton>
-    </>
+      <div>
+        <LinkButton to="https://github.com/featuredrift/featuredrift-web/fork">
+          Hack the Skin
+        </LinkButton>
+      </div>
+      <div>
+        <LinkButton to="https://github.com/featuredrift/featuredrift-web/issues/new?labels=enhancement">
+          Petition for Drift
+        </LinkButton>
+      </div>
+    </div>
   );
 }
 
 function HomeUserView({ session }: { session: SessionResponse }) {
   return (
     <>
-      <code>
+      <div className="absolute top-4 right-4">
+        <a
+          href="/auth/logout"
+          target="_self"
+          className="px-2 vertical-align-baseline inline-block  text-2xl border-2 border-cyan-600 text-purple-600 hover:border-transparent active:border-transparent hover:bg-purple-600 hover:text-cyan-400 active:text-cyan-400 active:bg-purple-700 text-center cursor-pointer"
+        >
+          X
+        </a>
+      </div>
+      <code className="text-xs">
         <pre>{JSON.stringify(session, null, 2)}</pre>
       </code>
-      <LinkButton to="/auth/logout" target="_self">
-        Logout
-      </LinkButton>
     </>
   );
 }
@@ -72,35 +95,20 @@ function HomeUserView({ session }: { session: SessionResponse }) {
 function MainLayout({ sessionPromise }: { sessionPromise: SessionPromise }) {
   const session = use(sessionPromise);
   const isLoggedIn = session?.user !== undefined;
+  const view =
+    session?.user !== undefined ? (
+      <HomeUserView session={session} />
+    ) : (
+      <UserLoginView />
+    );
 
   return (
     <div
-      className={classNames('p-8 h-full overflow-auto', {
+      className={classNames('p-8 h-full overflow-auto relative', {
         'border-4 border-purple-700': isLoggedIn,
       })}
     >
-      <div className="flex flex-col gap-8 justify-center items-center min-h-full">
-        <div className="flex flex-row justify-center align-middle">
-          <img
-            src={art}
-            alt="FeatureDrift AI Art"
-            className="max-h-[50vh] w-auto motion-safe:animate-pulse"
-          />
-        </div>
-
-        {isLoggedIn ? <HomeUserView session={session} /> : <UserLoginView />}
-
-        <div>
-          <LinkButton to="https://github.com/featuredrift/featuredrift-web/fork">
-            Hack the Skin
-          </LinkButton>
-        </div>
-        <div>
-          <LinkButton to="https://github.com/featuredrift/featuredrift-web/issues/new?labels=enhancement">
-            Petition for Drift
-          </LinkButton>
-        </div>
-      </div>
+      {view}
     </div>
   );
 }
