05d2ebf1c30727d5b9f7ba2f9ad15340a0f24bda
Alex Howard <thezanke@gmail.com>
2025-05-03 23:34:00 -0400

    linting

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,6 @@
 import axios from 'axios';
-import React, { HTMLAttributeAnchorTarget, PropsWithChildren, Suspense, use } from 'react';
+import type { HTMLAttributeAnchorTarget, PropsWithChildren } from 'react';
+import { Suspense, use } from 'react';
 import art from '/featuredrift-ai-art.png';
 
 function LinkButton({
@@ -7,22 +8,26 @@ function LinkButton({
   target = '_blank',
   children,
 }: PropsWithChildren<{
-  to: string,
-  target?: HTMLAttributeAnchorTarget
+  to: string;
+  target?: HTMLAttributeAnchorTarget;
 }>) {
   return (
-    <a href={to} target={target} className="p-4 inline-block rounded-4xl text-xl border-2 border-cyan-600 text-purple-600 hover:border-transparent hover:bg-purple-600 hover:text-cyan-400 active:bg-purple-700 text-center cursor-pointer hover:italic">
+    <a
+      href={to}
+      target={target}
+      className="p-4 inline-block rounded-4xl text-xl border-2 border-cyan-600 text-purple-600 hover:border-transparent hover:bg-purple-600 hover:text-cyan-400 active:bg-purple-700 text-center cursor-pointer hover:italic"
+    >
       {children}
     </a>
-  )
+  );
 }
 
 type SessionResponse = {
   message: string;
   user: {
     id: string;
-  }
-}
+  };
+};
 
 type SessionPromise = Promise<SessionResponse | null>;
 
@@ -37,46 +42,74 @@ async function getSession(): SessionPromise {
 }
 
 function SessionLoading() {
-  return (
-    <div className="text-2xl">
-      Loading...
-    </div>
-  )
+  return <div className="text-2xl">Loading...</div>;
 }
 
-function WithSessionPromise({ render }: { render: (sessionPromise: SessionPromise) => React.ReactNode }) {
+function WithSessionPromise({
+  render,
+}: {
+  render: (sessionPromise: SessionPromise) => React.ReactNode;
+}) {
   const sessionPromise = getSession().catch(() => null);
 
   return (
-    <Suspense fallback={<SessionLoading />}>
-      {render(sessionPromise)}
-    </Suspense>
-  )
+    <Suspense fallback={<SessionLoading />}>{render(sessionPromise)}</Suspense>
+  );
 }
 
-function LoginPortal({ sessionPromise }: { sessionPromise: SessionPromise }) {
-  const session = use(sessionPromise);
+function UserLoginView() {
+  return (
+    <div>
+      <LinkButton to="/auth/login" target="_self">
+        Login
+      </LinkButton>
+    </div>
+  );
+}
 
-  if (session === null) {
-    return (
-      <div>
-        <LinkButton to="/auth/login" target="_self">Login</LinkButton>
-      </div>
-    );
+function MainLayout({ sessionPromise }: { sessionPromise: SessionPromise }) {
+  const session = use(sessionPromise);
+  const isLoggedIn = session?.user !== undefined;
+
+  if (isLoggedIn) {
+    return <div></div>;
   }
 
   return (
-    <>
-      <code>
-        <pre>
-          {JSON.stringify(session, null, 2)}
-        </pre>
-      </code>
+    <div className="flex flex-col gap-8 justify-center items-center border-4 border-purple-700 p-8 h-full overflow-auto">
+      <div className="flex flex-row justify-center align-middle">
+        <img
+          src={art}
+          alt="FeatureDrift AI Art"
+          className="max-h-[50vh] w-auto motion-safe:animate-pulse"
+        />
+      </div>
+
+      {!isLoggedIn && <UserLoginView />}
 
       <div>
-        <LinkButton to="/auth/logout" target="_self">Logout</LinkButton>
+        {isLoggedIn && (
+          <LinkButton to="/auth/logout" target="_self">
+            Logout
+          </LinkButton>
+        )}
+        {!isLoggedIn && (
+          <LinkButton to="/auth/login" target="_self">
+            Login
+          </LinkButton>
+        )}
       </div>
-    </>
+      <div>
+        <LinkButton to="https://github.com/featuredrift/featuredrift-web/fork">
+          Hack the Skin
+        </LinkButton>
+      </div>
+      <div>
+        <LinkButton to="https://github.com/featuredrift/featuredrift-web/issues/new?labels=enhancement">
+          Petition for Drift
+        </LinkButton>
+      </div>
+    </div>
   );
 }
 
@@ -84,21 +117,10 @@ function App() {
   return (
     <WithSessionPromise
       render={(sessionPromise) => (
-        <div className="flex flex-col gap-8 justify-center items-center">
-          <div className="flex flex-row justify-center align-middle">
-            <img src={art} alt="FeatureDrift AI Art" className="max-h-[50vh] w-auto motion-safe:animate-pulse " />
-          </div>
-          <LoginPortal sessionPromise={sessionPromise} />
-          <div>
-            <LinkButton to="https://github.com/featuredrift/featuredrift-web/fork">Hack the Skin</LinkButton>
-          </div>
-          <div>
-            <LinkButton to="https://github.com/featuredrift/featuredrift-web/issues/new?labels=enhancement">Petition for Drift</LinkButton>
-          </div>
-        </div>
+        <MainLayout sessionPromise={sessionPromise} />
       )}
     />
-  )
+  );
 }
 
-export default App
+export default App;
