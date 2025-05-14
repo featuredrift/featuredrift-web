d1b97937d414ea4a78d8e8b2879421c1c0fb3f6a
Alex Howard <thezanke@gmail.com>
2025-05-14 10:36:23 -0400

    refactor: update layout and styling in NodeInfoPane for consistency; adjust CSS properties and improve structure

--- src/game/node-info-pane.tsx
+++ src/game/node-info-pane.tsx
@@ -8,17 +8,15 @@ export function NodeInfoPane({ node }: { node: NodeDetails | null }) {
   const safetyRatingColor = getSafetyRatingColor(node.safetyRating);
 
   return (
-    <div className="col-span-3 bg-purple-600 cornerless-b p-[1px] flex flex-col items-stretch max-h-40 md:max-h-80 xl:max-h-max">
-      <div className="flex flex-col cornerless-b bg-dark-bg overflow-hidden">
-        <div
-          className="text-xl sm:text-2xl md:text-3xl p-2 border-b-1 border-purple-700"
-          style={{ color: safetyRatingColor }}
-        >
-          {paddedId} - {node.name}
-        </div>
-        <div className="text-purple-300 font-light whitespace-pre-wrap text-sm sm:text-base italic p-2 overflow-auto grow">
-          {node.description}
-        </div>
+    <div className="col-span-3 bg-purple-600 cornerless-b flex flex-col justify-items-end cornerless-b overflow-hidden min-h-40 p-[1px] gap-[1px]">
+      <div
+        className="text-xl sm:text-2xl md:text-3xl p-2 bg-dark-bg"
+        style={{ color: safetyRatingColor }}
+      >
+        {paddedId} - {node.name}
+      </div>
+      <div className="text-purple-300 font-light whitespace-pre-wrap text-sm sm:text-base italic p-2 overflow-auto cornerless-b bg-dark-bg grow">
+        {node.description}
       </div>
     </div>
   );
